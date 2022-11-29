0b753416076dc7603d5f473850c670392fff8d69
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-29 17:38:08 +0900

    feat: 동적 페이지 추가 실습

---
+++ pages/home.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link href="/post/abc">Go to pages/post/[pid].js</Link>
+      </li>
+      <li>
+        <Link href="/post/abc?foo=bar">Also goes to pages/post/[pid].js</Link>
+      </li>
+      <li>
+        <Link href="/post/abc/a-comment">
+          Go to pages/post/[pid]/[comment].js
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Home
